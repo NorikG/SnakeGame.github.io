a33ce79903b7a7adb6f479586a4b819b36493ce4
NorikG <98643017+NorikG@users.noreply.github.com>
2022-01-29 19:32:22 +0900

    Add files via upload

---
+++ Змейка/управление.js
@@ -0,0 +1,28 @@
+document.addEventListener('keydown', function (e) {
+
+
+
+    if(e.which === 37 && snake.dx === 0) {
+
+
+
+            snake.dy = -grid
+            snake.dx = 0;
+        }
+
+        else if (e.which === 38 && snake.dy === 0) {
+        snake.dy = -grid;
+        snake.dx = 0;
+        }
+
+
+        else if (e.which === 39 && snake.dx === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+        }
+
+        else if (e.which === 40 && snake.dy === 0) {
+        snake.dy = grid;
+        snake.dx = 0;
+        }
+    });
